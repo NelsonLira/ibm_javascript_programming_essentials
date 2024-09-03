b320f527616d0d7f2368d4aac06ecb4504516c32
NelsonLira <nelson-lira@hotmail.com>
2024-09-03 13:45:14 -0400

    weather lab practise task

--- weatherReport/weather_report_2.js
+++ weatherReport/weather_report_2.js
@@ -1,9 +1,8 @@
 function showweatherDetails(event) {
       event.preventDefault();
-       const lat = document.getElementById('lat').value;
-       const lon = document.getElementById('lon').value;
+       const city = document.getElementById('city').value;
       const apiKey = 'c4f86ece00bc8aa272652ac9065af12d'; // Replace 'YOUR_API_KEY' with your actual API key
-      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
       fetch(apiUrl)
         .then(response => response.json())
